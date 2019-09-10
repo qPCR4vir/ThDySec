49d93b454d2a3d5d58dcc83a4b418d9a03f9ad69
qPCR4vir <arielvina@yahoo.es>
2019-09-10 13:28:49 +0200

    new version v0.02.10

--- src/ThDy_DNAHybrid.Nana/main.Nana.cpp
+++ src/ThDy_DNAHybrid.Nana/main.Nana.cpp
@@ -32,14 +32,14 @@
  Program distributed under the <bold blue url="https://www.gnu.org/licenses/"> GNU General Public License</>)" };
 
      nana::group build {*this, "Programm:"};
-     nana::label  compiled{ build, R"(Compiled on:   )"  __DATE__   R"( / )"  __TIME__   R"(    Version: v0.02.07     )" };
+     nana::label  compiled{ build, R"(Compiled on:   )"  __DATE__   R"( / )"  __TIME__   R"(    Version: v0.02.10     )" };
      nana::label  downloads{ build, R"(Downloads and source code: <bold blue url="https://github.com/qPCR4vir/ThDySec"> github.com/qPCR4vir/ThDySec</>
                                              <bold blue url=" https://github.com/qPCR4vir/ThDySec/wiki "> Wiki:</>
    ________________________________________________________________________________)" };
 
      nana::label  GUI_lib{ build, R"(Powered by <bold blue url="http://nanapro.org/en-us/ "> Nana C++ GUI library:</>
                                         <bold blue url="https://github.com/qPCR4vir/nana-docs/wiki"> Wiki:</>
-                      <bold blue url=" https://github.com/qPCR4vir/nana/">  Nana Version: 1.7.2 develop</>)" };
+                      <bold blue url="https://github.com/qPCR4vir/nana/tree/ed04f84d17985adf45fcfcfaac8b17abb5e391dc">  Nana Version: 1.7.2 dev</>)" };
 
 
 	 
